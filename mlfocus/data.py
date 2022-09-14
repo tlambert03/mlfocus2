1ae222cbe4469504c68858356f14392a68d99d1e
Talley Lambert <talley.lambert@gmail.com>
2022-09-14 16:22:12 -0400

    initial

---
+++ mlfocus/data.py
@@ -0,0 +1,103 @@
+from typing import Any, Callable, Optional
+from torch.utils.data import Dataset
+from torchvision import transforms as tf
+
+from pathlib import Path
+import zarr
+import torch
+import re
+
+
+class SheetAlign(Dataset):
+    """
+
+    Args:
+        root (string): Root directory of dataset where ``MNIST/raw/train-images-idx3-ubyte``
+            and  ``MNIST/raw/t10k-images-idx3-ubyte`` exist.
+        train (bool, optional): If True, creates dataset from ``train-images-idx3-ubyte``,
+            otherwise from ``t10k-images-idx3-ubyte``.
+        download (bool, optional): If True, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+    """
+
+    classes = [
+        "0 - beads",
+        "1 - diffuse",
+        "2 - mt",
+    ]
+
+    def __init__(
+        self,
+        root: str = "~/Downloads/mlfocus/zarr",
+        train: bool = True,
+        transforms: Optional[Callable] = None,
+        patch_size: int = 64,
+        download: bool = False,
+    ) -> None:
+        self.root = Path(root).expanduser()
+        self.patch_size = patch_size
+        if transforms is None:
+            transforms = tf.Compose([tf.ToTensor(), tf.RandomHorizontalFlip()])
+        self.transforms = transforms
+
+        if download:
+            self.download()
+
+        if not self._check_exists():
+            raise RuntimeError(
+                "Dataset not found. You can use download=True to download it"
+            )
+
+        self.arrays, self.keys, self.targets = self._load_data()
+        self.classes = []
+        for a in self.arrays:
+            clsname = re.split("\d", Path(a.store.path).stem)[0]
+            self.classes.extend([clsname] * len(a))
+
+    def __len__(self):
+        return len(self.targets)
+
+    def _check_exists(self) -> bool:
+        return self.root.exists()
+
+    def _load_data(self):
+        arrays = [zarr.open(i)["raw"] for i in sorted(self.root.glob("*zarr"))]
+        keys = [(i, x) for i, a in enumerate(arrays) for x in range(len(a))]
+        targets = [(s - 25) / 10 for _, s in keys]
+        return arrays, keys, targets
+
+    def __getitem__(self, index: int) -> tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        (file, focus), target = self.keys[index], self.targets[index]
+
+        img = self.arrays[file][focus]
+
+        z, h, w = img.shape
+        tz, th, tw = (self.patch_size,) * 3
+
+        if z + 1 < tz or h + 1 < th or w + 1 < tw:
+            raise ValueError(
+                f"Required crop size {(tz, th, tw)} is larger then input image size {(z, h, w)}"
+            )
+
+        deep = torch.randint(0, z - tz + 1, size=(1,)).item()
+        top = torch.randint(0, h - th + 1, size=(1,)).item()
+        left = torch.randint(0, w - tw + 1, size=(1,)).item()
+
+        img = img[deep : deep + tz, top : top + th, left : left + tw].astype("float32")
+
+        if self.transforms is not None:
+            img = self.transforms(img)
+
+        return img, target
